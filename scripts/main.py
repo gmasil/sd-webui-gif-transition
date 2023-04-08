d65385ebe142f66d260d9566648ef3f603ef56d1
gmasil <54176035+gmasil@users.noreply.github.com>
2023-04-08 03:37:28 +0200

    allow recreation of gif without processing images again

--- scripts/main.py
+++ scripts/main.py
@@ -25,19 +25,19 @@ def build_prompts(original_prompt: str, start_tag: str, end_tag: str, bias_min: 
     return prompts
 
 
-def make_gif(frames, filename=None, frame_duration=100):
+def make_gif(frames, animation_duration: int):
     """Function to generate an animated GIF from the given frames and saves it to a generic output directory"""
-    if filename is None:
-        filename = str(uuid.uuid4())
+    filename: str = str(uuid.uuid4())
 
     outpath = "/output/gif-transition"
     if not os.path.exists(outpath):
         os.makedirs(outpath)
 
+    frame_duration: int = int(animation_duration/len(frames))
     first_frame, append_frames = frames[0], frames[1:]
     full_file_path: str = f"{outpath}/{filename}.gif"
     first_frame.save(full_file_path, format="GIF", append_images=append_frames, save_all=True, duration=frame_duration, loop=0)
-    return full_file_path
+    return [full_file_path]
 
 
 class GifTransitionExtension(scripts.Script):
@@ -50,7 +50,6 @@ class GifTransitionExtension(scripts.Script):
         self.stored_all_prompts = []
         self.stored_infotexts = []
         self.stored_seeds = []
-        self.last_generated_gif = None
 
     def title(self):
         """Define extension title"""
@@ -60,12 +59,17 @@ class GifTransitionExtension(scripts.Script):
         """Always show the script in txt2img and img2img tabs"""
         return scripts.AlwaysVisible
 
+    def create_gif(self, animation_duration):
+        """Function to generate an animated GIF from the given frames and saves it to a generic output directory"""
+        return make_gif(self.stored_images, animation_duration)
+
     def ui(self, _is_img2img):
         """Generate gradio UI"""
         with gr.Group():
             with gr.Accordion("GIF Transition", open=False, elem_id="giftransition"):
                 with gr.Row():
                     gr_enabled: bool = gr.Checkbox(label='Enable', value=False)
+                    gr_only_recreate_gif: bool = gr.Checkbox(label='Only recreate gif', value=False)
                 with gr.Row():
                     gr_start_tag: str = gr.Textbox(label="Start tag", value="short hair")
                     gr_end_tag: str = gr.Textbox(label="End tag", value="long hair")
@@ -76,24 +80,34 @@ class GifTransitionExtension(scripts.Script):
                     gr_image_count: int = gr.Number(label="Amount of frames", value=12)
                     gr_gif_duration: int = gr.Number(label="Total animation duration (in ms)", value=1000)
 
-        return [gr_enabled, gr_start_tag, gr_end_tag, gr_bias_min, gr_bias_max, gr_image_count, gr_gif_duration]
+        return [gr_enabled, gr_only_recreate_gif, gr_start_tag, gr_end_tag, gr_bias_min, gr_bias_max, gr_image_count, gr_gif_duration]
 
-    def process(self, p, gr_enabled: bool, gr_start_tag: str, gr_end_tag: str, gr_bias_min: float, gr_bias_max: float, gr_image_count: int, gr_gif_duration: int):
+    def process(self, p, gr_enabled: bool, gr_only_recreate_gif: bool, gr_start_tag: str, gr_end_tag: str, gr_bias_min: float, gr_bias_max: float, gr_image_count: int, _gr_gif_duration: int):
         """Main process function to intercept image generation"""
+
         if not gr_enabled or self.working:
+            # prevent endless loop
             return
 
+        if gr_enabled and gr_only_recreate_gif and len(self.stored_images) > 0:
+            # skip processing, only generate gif in postprocess
+            p.n_iter = 0
+            return
+
+        # enforce int type
         gr_image_count = int(gr_image_count)
 
         if gr_image_count <= 0:
+            # skip processing if no images should be generated
+            p.n_iter = 0
             return
 
+        # reset state
         self.working = True
         self.stored_images = []
         self.stored_all_prompts = []
         self.stored_infotexts = []
         self.stored_seeds = []
-        self.last_generated_gif = None
 
         p.batch_size = 1
         p.n_iter = 1
@@ -124,23 +138,26 @@ class GifTransitionExtension(scripts.Script):
         if len(frame_prompts) * 2 == len(self.stored_images):
             del self.stored_images[1::2]
 
-        frame_duration: int = int(gr_gif_duration/len(frame_prompts))
-        self.last_generated_gif = [make_gif(self.stored_images, frame_duration=frame_duration)]
-
         self.working = False
         p.n_iter = 0
 
-    def postprocess(self, _p, processed, gr_enabled: bool, _gr_start_tag: str, _gr_end_tag: str, _gr_bias_min: float, _gr_bias_max: float, _gr_image_count: int, _gr_gif_duration: int):
+    def postprocess(self, _p, processed, gr_enabled: bool, gr_only_recreate_gif: bool, _gr_start_tag: str, _gr_end_tag: str, _gr_bias_min: float, _gr_bias_max: float, gr_image_count: int, gr_gif_duration: int):
         """Function to collect all generated images after processing is done"""
-        if not gr_enabled or self.working:
+
+        if self.working:
             return
 
-        processed.images = self.stored_images
-        processed.all_prompts = self.stored_all_prompts
-        processed.infotexts = self.stored_infotexts
-        processed.all_seeds = self.stored_seeds
+        if (not gr_enabled or not gr_only_recreate_gif or len(self.stored_images) == 0) and (not gr_enabled or self.working or gr_image_count <= 0):
+            # skip if not required
+            return
 
-        processed.images += self.last_generated_gif
-        processed.all_prompts += ["GIF Transition Result"]
-        processed.infotexts += ["GIF Transition Result"]
-        processed.all_seeds += [-1]
+        processed.images = self.stored_images.copy()
+        processed.all_prompts = self.stored_all_prompts.copy()
+        processed.infotexts = self.stored_infotexts.copy()
+        processed.all_seeds = self.stored_seeds.copy()
+
+        if len(self.stored_images) > 0 and len(self.stored_images) == gr_image_count:
+            processed.images += self.create_gif(gr_gif_duration)
+            processed.all_prompts += ["GIF Transition Result"]
+            processed.infotexts += ["GIF Transition Result"]
+            processed.all_seeds += [-1]
